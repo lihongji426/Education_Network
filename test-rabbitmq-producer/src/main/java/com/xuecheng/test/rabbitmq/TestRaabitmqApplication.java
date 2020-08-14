78e6a3ee8e519bb501e34e84d619234e029bdb14
lihongji <3117313295@qq.com>
2020-08-14 17:11:52 +0800

    test rabbitMQ

---
+++ test-rabbitmq-producer/src/main/java/com/xuecheng/test/rabbitmq/TestRaabitmqApplication.java
@@ -0,0 +1,12 @@
+package com.xuecheng.test.rabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestRaabitmqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestRaabitmqApplication.class, args);
+    }
+}
